8f4a736327e4cf21635ecac204e1959ddeaf71fd
AmarDilig <amar.kajevic@dilig.net>
2025-12-08 00:24:27 +0100

    fixing cors 1

--- routes/auth.js
+++ routes/auth.js
@@ -13,6 +13,22 @@ import {
 import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
+router.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://medikalija-frontend.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 
 // ---------------- AUTH ----------------
 router.post("/login", login);
